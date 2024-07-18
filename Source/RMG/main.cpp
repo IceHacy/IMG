f6cce920dd73a9bdba1ec2518c3288986d3595bb
IceHacy <78807447+IceHacy@users.noreply.github.com>
2024-07-18 16:27:15 +0100

    main.cpp

--- Source/RMG/main.cpp
+++ Source/RMG/main.cpp
@@ -1,19 +1,8 @@
-/*
- * Rosalie's Mupen GUI - https://github.com/Rosalie241/RMG
- *  Copyright (C) 2020 Rosalie Wanders <rosalie@mailbox.org>
- *
- *  This program is free software: you can redistribute it and/or modify
- *  it under the terms of the GNU General Public License version 3.
- *  You should have received a copy of the GNU General Public License
- *  along with this program. If not, see <https://www.gnu.org/licenses/>.
- */
 #include <UserInterface/MainWindow.hpp>
-
 #include <QApplication>
 #include <QCommandLineParser>
 #include <QFile>
 #include <QDir>
-
 #include <RMG-Core/Core.hpp>
 #include <iostream>
 #include <cstdlib>
@@ -22,126 +11,65 @@
 #include <signal.h>
 #endif
 
-//
-// Local Functions
-//
-
-void message_handler(QtMsgType type, const QMessageLogContext &context, const QString &msg)
-{
+void message_handler(QtMsgType type, const QMessageLogContext &context, const QString &msg) {
     QByteArray localMsg = msg.toLocal8Bit();
     bool showDebugQtMessages = false;
-
     const char* env = std::getenv("RMG_SHOW_DEBUG_QT_MESSAGES");
     showDebugQtMessages = env != nullptr && std::string(env) == "1";
 
     std::string typeString;
-
-    switch (type)
-    {
-    case QtDebugMsg:
-        if (!showDebugQtMessages)
-        {
-            return;
-        }
-        typeString = "[QT DEBUG] ";
-        break;
-    case QtWarningMsg:
-        if (!showDebugQtMessages)
-        {
-            return;
-        }
-        typeString = "[QT WARNING] ";
-        break;
-    case QtInfoMsg:
-        if (!showDebugQtMessages)
-        {
-            return;
-        }
-        typeString = "[QT INFO] ";
-        break;
-    case QtCriticalMsg:
-        typeString = "[QT CRITICAL] ";
-        break;
-    case QtFatalMsg:
-        typeString = "[QT FATAL] ";
-        break;
+    switch (type) {
+        case QtDebugMsg:
+        case QtWarningMsg:
+        case QtInfoMsg:
+            if (!showDebugQtMessages) return;
+            typeString = (type == QtDebugMsg) ? "[QT DEBUG] " :
+                         (type == QtWarningMsg) ? "[QT WARNING] " : "[QT INFO] ";
+            break;
+        case QtCriticalMsg:
+            typeString = "[QT CRITICAL] ";
+            break;
+        case QtFatalMsg:
+            typeString = "[QT FATAL] ";
+            break;
     }
-
     std::cerr << typeString << localMsg.constData() << std::endl;
 }
 
-void signal_handler(int sig)
-{
+void signal_handler(int sig) {
     QGuiApplication::quit();
 }
 
-//
-// Exported Functions
-//
-
-int main(int argc, char **argv)
-{
-    // install message handler
+int main(int argc, char **argv) {
     qInstallMessageHandler(message_handler);
 
 #ifndef _WIN32
-    // on Linux we need to install signal handlers,
-    // so we can exit cleanly when the user presses
-    // i.e control+c
-    signal(SIGINT,  signal_handler);
+    signal(SIGINT, signal_handler);
     signal(SIGTERM, signal_handler);
 
-    // on Linux, wayland works only on some compositors,
-    // it works on KDE plasma and sway (on 2023-07-26),
-    // but i.e doesn't work on GNOME wayland or labwc, 
-    // so to compromise the situation, we'll force xwayland
-    // unless RMG_WAYLAND is set to 1, which'll force wayland
-    // as qt platform, so users can experiment with the
-    // wayland support themselves
     const char* wayland = std::getenv("RMG_WAYLAND");
-    if (wayland != nullptr &&
-        std::string(wayland) == "1")
-    {
+    if (wayland != nullptr && std::string(wayland) == "1") {
         setenv("QT_QPA_PLATFORM", "wayland", 1);
-    }
-    else
-    {
+    } else {
         setenv("QT_QPA_PLATFORM", "xcb", 1);
     }
 
-    // on Linux, Qt on some distributions
-    // fails to load libvulkan, so to fix that
-    // we'll help out and tell it about libvulkan.so.1,
-    // but we wont overwrite the variable value if it
-    // already contains one, the user might set it
-    // to a custom value themselves
     setenv("QT_VULKAN_LIB", "libvulkan.so.1", 0);
 
-    // ensure the default OpenGL format
-    // doesn't have vsync enabled by default,
-    // only needed for linux (for now)
-    // and ensure OpenGL 3.3 is specified by default (for wayland)
     QSurfaceFormat format = QSurfaceFormat::defaultFormat();
     format.setSwapInterval(0);
     format.setMajorVersion(3);
     format.setMinorVersion(3);
     QSurfaceFormat::setDefaultFormat(format);
 
-    // ensure that the desktop file is correctly
-    // specified, else the window icon will be
-    // the generic wayland icon on wayland
     QGuiApplication::setDesktopFileName("com.github.Rosalie241.RMG");
 #endif
 
     QApplication app(argc, argv);
-
     UserInterface::MainWindow window;
 
 #ifdef PORTABLE_INSTALL
-    // only change current directory
-    // when we're in portable directory mode
-    if (CoreGetPortableDirectoryMode())
-    {
+    if (CoreGetPortableDirectoryMode()) {
         QDir::setCurrent(app.applicationDirPath());
     }
 #endif
@@ -149,12 +77,10 @@ int main(int argc, char **argv)
     QCoreApplication::setApplicationName("Rosalie's Mupen GUI");
     QCoreApplication::setApplicationVersion(QString::fromStdString(CoreGetVersion()));
 
-    // setup commandline parser
     QCommandLineParser parser;
-    // default options
     parser.addHelpOption();
     parser.addVersionOption();
-    // custom options
+
 #ifndef PORTABLE_INSTALL
     QCommandLineOption libPathOption("lib-path", "Changes the path where the libraries are stored", "path");
     QCommandLineOption corePathOption("core-path", "Changes the path where the core library is stored", "path");
@@ -164,7 +90,8 @@ int main(int argc, char **argv)
     corePathOption.setFlags(QCommandLineOption::HiddenFromHelp);
     pluginPathOption.setFlags(QCommandLineOption::HiddenFromHelp);
     sharedDataPathOption.setFlags(QCommandLineOption::HiddenFromHelp);
-#endif // PORTABLE_INSTALL
+#endif
+
     QCommandLineOption debugMessagesOption({"d", "debug-messages"}, "Prints debug callback messages to stdout");
     QCommandLineOption fullscreenOption({"f", "fullscreen"}, "Launches ROM in fullscreen mode");
     QCommandLineOption noGuiOption({"n", "nogui"}, "Hides GUI elements (menubar, toolbar, statusbar)");
@@ -177,7 +104,7 @@ int main(int argc, char **argv)
     parser.addOption(corePathOption);
     parser.addOption(pluginPathOption);
     parser.addOption(sharedDataPathOption);
-#endif // PORTABLE_INSTALL
+#endif
     parser.addOption(debugMessagesOption);
     parser.addOption(fullscreenOption);
     parser.addOption(noGuiOption);
@@ -186,63 +113,37 @@ int main(int argc, char **argv)
     parser.addOption(diskOption);
     parser.addPositionalArgument("ROM", "ROM to open");
 
-    // parse arguments
     parser.process(app);
 
 #ifndef PORTABLE_INSTALL
-    // set path overrides before initializing
-    QString libPathOverride        = parser.value(libPathOption);
-    QString corePathOveride        = parser.value(corePathOption);
-    QString pluginPathOverride     = parser.value(pluginPathOption);
+    QString libPathOverride = parser.value(libPathOption);
+    QString corePathOveride = parser.value(corePathOption);
+    QString pluginPathOverride = parser.value(pluginPathOption);
     QString sharedDataPathOverride = parser.value(sharedDataPathOption);
-    if (!libPathOverride.isEmpty())
-    {
-        CoreSetLibraryPathOverride(libPathOverride.toStdString());
-    }
-    if (!corePathOveride.isEmpty())
-    {
-        CoreSetCorePathOverride(corePathOveride.toStdString());
-    }
-    if (!pluginPathOverride.isEmpty())
-    {
-        CoreSetPluginPathOverride(pluginPathOverride.toStdString());
-    }
-    if (!sharedDataPathOverride.isEmpty())
-    {
-        CoreSetSharedDataPathOverride(sharedDataPathOverride.toStdString());
-    }
-#endif // PORTABLE_INSTALL
+    if (!libPathOverride.isEmpty()) CoreSetLibraryPathOverride(libPathOverride.toStdString());
+    if (!corePathOveride.isEmpty()) CoreSetCorePathOverride(corePathOveride.toStdString());
+    if (!pluginPathOverride.isEmpty()) CoreSetPluginPathOverride(pluginPathOverride.toStdString());
+    if (!sharedDataPathOverride.isEmpty()) CoreSetSharedDataPathOverride(sharedDataPathOverride.toStdString());
+#endif
 
-    // print debug callbacks to stdout if needed
     CoreSetPrintDebugCallback(parser.isSet(debugMessagesOption));
-
-    // specified ROM path to launch
     QStringList args = parser.positionalArguments();
 
-    CoreAddCallbackMessage(CoreDebugMessageType::Info, 
-            "Initializing on " + QGuiApplication::platformName().toStdString());
+    CoreAddCallbackMessage(CoreDebugMessageType::Info, "Initializing on " + QGuiApplication::platformName().toStdString());
 
-    // initialize window
-    if (!window.Init(&app, !parser.isSet(noGuiOption), !args.empty()))
-    {
+    if (!window.Init(&app, !parser.isSet(noGuiOption), !args.empty())) {
         return 1;
     }
 
-    if (!args.empty())
-    {
+    if (!args.empty()) {
         bool parsedNumber = false;
         int saveStateSlot = parser.value(loadStateSlot).toInt(&parsedNumber);
-        if (parser.value(loadStateSlot).isEmpty() || !parsedNumber ||
-            saveStateSlot < 0 || saveStateSlot > 9)
-        {
+        if (parser.value(loadStateSlot).isEmpty() || !parsedNumber || saveStateSlot < 0 || saveStateSlot > 9) {
             saveStateSlot = -1;
         }
-
         window.OpenROM(args.at(0), parser.value(diskOption), parser.isSet(fullscreenOption), parser.isSet(quitAfterEmulationOption), saveStateSlot);
     }
 
-    // show window
     window.show();
-
     return app.exec();
 }
